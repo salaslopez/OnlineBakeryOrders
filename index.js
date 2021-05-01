f429825a8e4a07b667eaff3fbee13d1be4c48cb2
zaraysalas <salas-lopez@hotmail.com>
2021-05-01 18:00:27 +0000

    EJS

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ express = require('express'),
 bodyParser = require('body-parser'),
 mongoose = require('mongoose'),//Allows to connect to the DB
 dotenv = require('dotenv'),
-path = require('path');
+path = require('path'),
+request = require('request');
 var FinalProduct = require('./models/finalproduct');
 var app = express();
 
@@ -15,25 +16,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('tiny'));
 
 
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(express.urlencoded({extended: false}));
-
 app.use(express.json());
-//Opening HTML-Online Bakery
-app.use(express.static('front-end'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('views/index'));//Opening HTML-Online Bakery
+//app.set('views',path.resolve(__dirname, 'views'));
 
 app.get('/', async function(req, res) {
+    console.log("HELLO FROM INDEX");
     const allproducts = await FinalProduct.find();
-    res.render('index', {allproducts: allproducts});
-    console.log(allproducts);
+    res.render('index',{allproducts: allproducts});
+    console.log("allproducts: " + allproducts);
+});
+
+
+
+
+app.get('/searching', function (req, res) {
+    
+    var val = req.query.search;
+    console.log(val);
+res.send("WHEEE");
+    // var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search" +
+    //     "%20where%20location%3D%22sfbay%22%20and%20type%3D%22jjj%22%20and%20query%3D%22" + val + "%22&format=" +
+    //     "json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    // console.log(url);
+
+    // request(url, function (err, resp, body) {
+    //  console.log(body);
+    //      this.body = JSON.parse(body);
+        
+    //     // logic used to compare search results with the input from user
+    //     if (!body.query.results.RDF.item) {
+    //         craig = "No results found. Try again.";
+    //     } else {
+    //         craig = body.query.results.RDF.item[0]['about'];
+    //     }
+    //     // pass back the results to client side
+    //     res.send(craig);
+    //});
+
 });
 
 //------------Server-----------
 
-var port = 8000;//process.env.PORT || 8000;
+var port = process.env.PORT || 8000;
 dotenv.config();
 
 
---
+++ views/js/OnlineBakerytable.js
@@ -0,0 +1,67 @@
+$(function(){
+ $('#search').on('keyup', function(e){
+   $( "#searchby" ).click(function() {
+  alert( "Handler for .click() called." );
+     var parameters = { search: $(this).val() };
+       $.get( '/searching',parameters, function(data) {
+       $('#results').html(data);
+     });
+    });
+ });
+});
+//Display the table in the HTML
+function draw_table()
+{
+	//$("#tablewithall").empty();
+	$.getJSONuncached = function (url)
+	{
+		return $.ajax(
+		{
+			url: url,
+			type: 'GET',
+			cache: false,
+			success: function (html)
+			{
+				$("#tablewithall").append(html);
+				select_row();
+			}
+		});
+	};
+	$.getJSONuncached("/finalproducts")
+};
+//<!-- This function takes the information provided when the row is selected and sends to the next function
+// function select_row()
+// {
+// 	$("#menuTable tbody tr[id]").click(function ()
+// 	{
+// 		$(".selected").removeClass("selected");
+// 		$(this).addClass("selected");
+// 		var section = $(this).prevAll("tr").children("td[colspan='4']").length - 1;
+// 		var finalProduct = $(this).attr("id") - 1;
+// 		delete_row(section, finalProduct);
+// 	})
+// };
+//Send back to the fron-end the informationn after update it with the deleted one.
+// function delete_row(sec, ent)
+// {
+// 	$("#delete").click(function ()
+// 	{
+// 		$.ajax(
+// 		{
+// 			url: "/post/delete",
+// 			type: "POST",
+// 			data:
+// 			{
+// 				section: sec,
+// 				finalProduct: ent
+// 			},
+// 			cache: false,
+// 			success: setTimeout(draw_table, 1000)
+// 		})
+// 	})
+// };
+// // Call the function that display the table in real-time without having to reload the page
+$(document).ready(function ()
+{
+	draw_table();
+});
