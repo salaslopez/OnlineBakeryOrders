ff804919cd7fbcfd15d96396f581b9c69abf2673
salaslopez <salaszaray@gmail.com>
2021-05-04 19:43:49 +0000

    Comments

--- finalproduct-controller.js
+++ finalproduct-controller.js
@@ -1,43 +1,22 @@
 //Functions define in "routes.js"
 var FinalProduct = require('./models/finalproduct');
 
-//Capture all the data in DB
-exports.index = async function(req, res) {
-    const allproducts = await FinalProduct.find();
-    res.render('index',{allproducts: allproducts});
-};
+
 //-------------Create-----------------
-exports.createFinalProduct = function(req, res){
+exports.createFinalProduct = function (req, res) {
     var newfinalproduct = new FinalProduct(req.body);
-    newfinalproduct.save(function(err, finalproduct){
-        if(err){
+    newfinalproduct.save(function (err, finalproduct) {
+        if (err) {
             res.status(400).json(err);
         }
         res.json(finalproduct);
     });
 };
 
-exports.createProduct = function(req, res){
-    var finalproduct = new FinalProduct();
-    finalproduct.section = req.body.section;
-    finalproduct.mostWanted = req.body.mostwanted;
-    finalproduct.item = req.body.item;
-    finalproduct.ingredient = req.body.ingredient;
-    finalproduct.quantity = req.body.quantity;
-    finalproduct.unit = req.body.unit;
-    finalproduct.save(function(err){
-            if(err){
-                console.log(err);
-                return;
-            }
-            res.redirect('/');
-        });
-};
-
 //-------------Get all finalproducts-------------------
-exports.getFinalProducts = function(req, res){
-    FinalProduct.find({}, function(err, finalproducts){
-        if(err){
+exports.getFinalProducts = function (req, res) {
+    FinalProduct.find({}, function (err, finalproducts) {
+        if (err) {
             res.status(400).json(err);
         }
         res.json(finalproducts);
@@ -46,9 +25,9 @@ exports.getFinalProducts = function(req, res){
 };
 
 //------------Get a item with the ingredient of your chosen manually in the url----------------------
-exports.getFinalProduct = function(req, res){
-    FinalProduct.findOne({ingredient: req.params.ingredient}, function(err, finalproducts){
-        if(err){
+exports.getFinalProduct = function (req, res) {
+    FinalProduct.findOne({ ingredient: req.params.ingredient }, function (err, finalproducts) {
+        if (err) {
             res.status(400).json(err);
         }
         res.json(finalproducts);
@@ -56,73 +35,100 @@ exports.getFinalProduct = function(req, res){
 };
 
 //-------------Updating finalproduct based on ID -----------------
-exports.updateFinalProduct = function(req, res){
-    FinalProduct.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function
-        (err, finalproducts){
-        if(err){
+exports.updateFinalProduct = function (req, res) {
+    FinalProduct.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, function
+        (err, finalproducts) {
+        if (err) {
             res.status(400).json(err);
         }
         res.json(finalproducts);
     });
-};
-
-exports.loadform = function(req, res){
-    FinalProduct.findById(req.params.id, function(error, article){
-        res.render('updateproduct',{
-            article:article
-        });
-    });
-};
-exports.updateProduct = function(req, res){
-    var article = {};
-    article.section = req.body.section;
-    article.mostWanted = req.body.mostwanted;
-    article.item = req.body.item;
-    article.ingredient = req.body.ingredient;
-    article.quantity = req.body.quantity;
-    article.unit = req.body.unit;
-console.log(article);
-    var checkId = {_id: req.params.id};
-    console.log(checkId);
-
-    FinalProduct.update(checkId, article, function(err){
-            if(err){
-                console.log(err);
-                return;
-            }
-            res.redirect('/');
-        });
 };
 
 //---------------Deleting finalproduct by ID-----------------------------
-exports.deleteFinalProduct = function(req, res){
-    FinalProduct.findByIdAndRemove({_id: req.params.id}, function(err, finalproducts){
-        if(err){
+exports.deleteFinalProduct = function (req, res) {
+    FinalProduct.findByIdAndRemove({ _id: req.params.id }, function (err, finalproducts) {
+        if (err) {
             res.status(400).json(err);
         }
         res.json(finalproducts);
     });
-};
-
-exports.deleteProduct = function(req, res){
-//var checkId = {_id: new mongodb.ObjectID(req.params.id)};
-var checkId = {_id: req.params.id};
-FinalProduct.remove(checkId, function(err){
-    if(err){
-        console.log(err);
-    }
-    res.send('Success')
-})
-
 };
 
 //------------------------------Find by  ID -----------------
-exports.updateFinalProduct = function(req, res){
-    FinalProduct.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true}, function
-        (err, finalproducts){
-        if(err){
+exports.updateFinalProduct = function (req, res) {
+    FinalProduct.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true }, function
+        (err, finalproducts) {
+        if (err) {
             res.status(400).json(err);
         }
         res.json(finalproducts);
     });
+};
+
+
+
+
+
+//----------- full commands that change the database in MongoDB Atlas with an API & WebSite
+//Capture all the data in DB
+exports.index = async function (req, res) {
+    const allproducts = await FinalProduct.find();
+    res.render('index', { allproducts: allproducts });
+};
+
+exports.createProduct = function (req, res) {
+    var finalproduct = new FinalProduct();
+    finalproduct.section = req.body.section;
+    finalproduct.mostWanted = req.body.mostwanted;
+    finalproduct.item = req.body.item;
+    finalproduct.ingredient = req.body.ingredient;
+    finalproduct.quantity = req.body.quantity;
+    finalproduct.unit = req.body.unit;
+    finalproduct.save(function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.redirect('/');
+    });
+};
+
+exports.loadform = function (req, res) {
+    FinalProduct.findById(req.params.id, function (error, finalproduct) {
+        res.render('updateproduct', {
+            finalproduct: finalproduct
+        });
+    });
+};
+exports.updateProduct = function (req, res) {
+    var finalproduct = {};
+    finalproduct.section = req.body.section;
+    finalproduct.mostWanted = req.body.mostwanted;
+    finalproduct.item = req.body.item;
+    finalproduct.ingredient = req.body.ingredient;
+    finalproduct.quantity = req.body.quantity;
+    finalproduct.unit = req.body.unit;
+
+    console.log(finalproduct);
+    var checkId = { _id: req.params.id };
+    console.log(checkId);
+
+    FinalProduct.update(checkId, finalproduct, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.redirect('/');
+    });
+};
+
+exports.deleteProduct = function (req, res) {
+    var checkId = { _id: req.params.id };
+    FinalProduct.remove(checkId, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        res.send('Success')
+    })
 };
--- routes.js
+++ routes.js
@@ -5,22 +5,23 @@ router = express.Router();
 
 var finalproductCtrl = require('./finalproduct-controller');
 
-//Show all the products in the inicial website
-router.get('/', finalproductCtrl.index);
-
+//---------------router to change the DB manually ------------------------------
 router.post('/finalproducts', finalproductCtrl.createFinalProduct);//Complement the end of the URL to create a new record
-router.post('/articles/add', finalproductCtrl.createProduct);
-
-//It is used in the index to see all the database
-router.get('/finalproducts', finalproductCtrl.getFinalProducts);//Complement the end of the URL to see all the records
 //This will show the items with the ingredient chosen
 router.get('/finalproducts/:ingredient', finalproductCtrl.getFinalProduct);
-router.get('/article/edit/:id', finalproductCtrl.loadform);
-router.post('/articles/edit/:id', finalproductCtrl.updateProduct);
-
 router.put('/finalproducts/:id', finalproductCtrl.updateFinalProduct);
 router.delete('/finalproducts/:id', finalproductCtrl.deleteFinalProduct);
 router.delete('/delete/:id', finalproductCtrl.deleteProduct);
+
+//--------- router to change the database dynamically trough API/MongoDB Atlas ---------------------------------
+//Show all the products in the inicial website
+router.get('/', finalproductCtrl.index);
+//It is used in the index to see all the database
+router.get('/finalproducts', finalproductCtrl.getFinalProducts);//Complement the end of the URL to see all the records
+router.post('/finalproduct/add', finalproductCtrl.createProduct);
+//Pass one product data to be updated
+router.get('/finalproduct/edit/:id', finalproductCtrl.loadform);
+router.post('/finalproduct/edit/:id', finalproductCtrl.updateProduct);
 
 
 
